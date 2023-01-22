07cd76481f6e8275c5d26bdebdf4be0c62214aa6
etghanibahman <51031083+etghanibahman@users.noreply.github.com>
2023-01-22 20:52:15 +0100

    MinMaxStackConstruction

--- MinMaxStackConstruction/Program.cs
+++ MinMaxStackConstruction/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MinMaxStackConstruction
 {
@@ -6,41 +7,82 @@ namespace MinMaxStackConstruction
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+			MinMaxStack testCase1 = new MinMaxStack();
+
+			testCase1.Push(5);
+            Console.WriteLine($"getMin after pushing 5:{testCase1.GetMin()}");
+			Console.WriteLine($"getMax after pushing 5:{testCase1.GetMax()}");
+			Console.WriteLine($"peek after pushing 5:{testCase1.Peek()}");
+			testCase1.Push(7);
+			Console.WriteLine($"getMin after pushing 7:{testCase1.GetMin()}");
+			Console.WriteLine($"getMax after pushing 7:{testCase1.GetMax()}");
+			Console.WriteLine($"peek after pushing 7:{testCase1.Peek()}");
+			testCase1.Push(2);
+			Console.WriteLine($"getMin after pushing 2:{testCase1.GetMin()}");
+			Console.WriteLine($"getMax after pushing 2:{testCase1.GetMax()}");
+			Console.WriteLine($"peek after pushing 2:{testCase1.Peek()}");
+		}
 
 		public class MinMaxStack
 		{
+			Stack<int> mainStack = new Stack<int>();
+			Stack<int> minStack = new Stack<int>();
+			Stack<int> maxStack = new Stack<int>();
+
 			public int Peek()
 			{
-				// Write your code here.
-				return -1;
+				return mainStack.Peek();
 			}
 
 			public int Pop()
 			{
-				// Write your code here.
-				return -1;
+				minStack.Pop();
+				maxStack.Pop();
+				return mainStack.Pop();
 			}
 
 
 			public void Push(int number)
 			{
-				// Write your code here.
+                if (mainStack.Count == 0)
+                {
+					mainStack.Push(number);
+					maxStack.Push(number);
+					minStack.Push(number);
+                }
+                else
+                {
+					int currMin = minStack.Peek();
+					int currMax = maxStack.Peek();
+					mainStack.Push(number);
+                    if (number > currMax)
+                    {
+						maxStack.Push(number);
+						minStack.Push(currMin);
+                    }
+                    else if (number < currMin)
+                    {
+						minStack.Push(number);
+						maxStack.Push(currMax);
+                    }
+                    else
+                    {
+						minStack.Push(currMin);
+						maxStack.Push(currMax);
+					}
+                }
 			}
 
 
 			public int GetMin()
 			{
-				// Write your code here.
-				return -1;
+				return minStack.Peek();
 			}
 
 
 			public int GetMax()
 			{
-				// Write your code here.
-				return -1;
+				return maxStack.Peek();
 			}
 		}
 	}
