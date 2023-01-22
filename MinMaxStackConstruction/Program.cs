dcd9463a7b0e4f720fbf76fa2c6ee3bf46441311
etghanibahman <51031083+etghanibahman@users.noreply.github.com>
2023-01-22 19:59:16 +0100

    Add project files.

---
+++ MinMaxStackConstruction/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace MinMaxStackConstruction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+		public class MinMaxStack
+		{
+			public int Peek()
+			{
+				// Write your code here.
+				return -1;
+			}
+
+			public int Pop()
+			{
+				// Write your code here.
+				return -1;
+			}
+
+
+			public void Push(int number)
+			{
+				// Write your code here.
+			}
+
+
+			public int GetMin()
+			{
+				// Write your code here.
+				return -1;
+			}
+
+
+			public int GetMax()
+			{
+				// Write your code here.
+				return -1;
+			}
+		}
+	}
+}
