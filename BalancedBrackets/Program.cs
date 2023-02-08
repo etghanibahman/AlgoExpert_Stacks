41b155aa85df3485b4f7ee30b20c431866f38a74
etghanibahman <51031083+etghanibahman@users.noreply.github.com>
2023-02-08 23:21:01 +0100

    organizing Stacks

---
+++ BalancedBrackets/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BalancedBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "([])(){ } (())()()";
+
+            Console.WriteLine($"is this string {str} balanced? {BalancedBrackets(str)}");
+        }
+
+        public static bool BalancedBrackets(string str)
+        {
+            // Write your code here.
+            return false;
+        }
+    }
+}
