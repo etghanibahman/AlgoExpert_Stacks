64435406f22556a56ed60fc4174e6df7f75624b7
etghanibahman <51031083+etghanibahman@users.noreply.github.com>
2023-02-09 23:20:54 +0100

    BalancedBrackets

--- BalancedBrackets/Program.cs
+++ BalancedBrackets/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace BalancedBrackets
 {
@@ -8,13 +9,38 @@ namespace BalancedBrackets
         {
             string str = "([])(){ } (())()()";
 
-            Console.WriteLine($"is this string {str} balanced? {BalancedBrackets(str)}");
+            str = "{[[[[({(}))]]]]}"; // test case 12 
+            str = ")[]}"; // test case 14
+
+            Console.WriteLine($"is this string {str} \nbalanced? {BalancedBrackets(str)}");
         }
 
+        //Time = O(N), Space = O(N)
         public static bool BalancedBrackets(string str)
         {
-            // Write your code here.
-            return false;
+            Stack<char> stackChars = new Stack<char>();
+            var openingChars = new char[] { '(', '[', '{' };
+            var closingChars = new char[] { ')', ']', '}' };
+            //We can also defice a dictionary for matching characters instead of comparing indexes 
+            foreach (var item in str)
+            {
+                if (Array.IndexOf(openingChars, item) >= 0)
+                {
+                    stackChars.Push(item);
+                }
+                else if (Array.IndexOf(closingChars, item) >= 0)
+                {
+                    if (stackChars.Count == 0)
+                        return false;
+                    var popedChar = stackChars.Pop();
+                    if (Array.IndexOf(openingChars, popedChar) != Array.IndexOf(closingChars, item))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return stackChars.Count == 0;
         }
     }
 }
