8dd4729c268760740d64fb91d14f78cbc6032e4c
etghanibahman <51031083+etghanibahman@users.noreply.github.com>
2023-06-08 13:50:38 +0200

    some correction in files places

---
+++ Medium/SortStack/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SortStack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
